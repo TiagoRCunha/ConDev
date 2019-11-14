f6596dc26162bdc1b1e3f5f40ff4ec82d10d83be
Victor Corassa <victorcorassa@MacBook-Pro-de-Victor.local>
2019-11-14 17:47:54 -0300

    Tela de Home

--- mobile/src/screens/Home/Home.js
+++ mobile/src/screens/Home/Home.js
@@ -1,44 +1,54 @@
 import React, { Component } from 'react';
-import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+import { View, Text, TextInput, ScrollView, Alert } from 'react-native';
 import Estilo from './Estilo'
 import { BotaoEntrar, BotaoCadastrar, BotaoEsqueciSenha, BotaoGoogle, BotaoLinkedin } from '../../components/Button'
 
 
 
 
-export default class mobile extends Component {
+export default class Home extends Component {
   render() {
     return (
-      <View style={Estilo.view}>
-        <View style={Estilo.image}>
-          <Text style={Estilo.text}>
-            ConDev
+      <ScrollView> 
+        <View style={Estilo.view}>
+          <View style={Estilo.image}>
+            <Text style={Estilo.text}>
+              ConDev
           </Text>
-        </View>
-        <View style={Estilo.view1}>
-          <View style={{ ...Estilo.textInput, marginBottom: 30 }}>
-            <TextInput
-              placeholder="Login"
+          </View>
+          <View style={Estilo.view1}>
+            <View style={{ ...Estilo.textInput, marginBottom: 30 }}>
+              <TextInput
+                placeholder="Login"
+              />
+            </View>
+            <View style={{ ...Estilo.textInput, marginBottom: 5 }}>
+              <TextInput
+                placeholder="Senha" />
+            </View>
+            <View style={{ flexDirection: 'row', width: '100%' }}>
+              <Text>                             </Text>
+              <BotaoEsqueciSenha
+              onPress={() => this.props.navigation.push('Senha')} />
+            </View>
+
+            <BotaoEntrar
+            onPress={() => Alert.alert('ConDev','Trabalhando fera')} />
+            <BotaoCadastrar
+              onPress={() => this.props.navigation.push('TipoCadastro')}
             />
+            <View style={Estilo.line}></View>
+            <View>
+              <BotaoGoogle 
+              onPress={() => this.props.navigation.push('Google')}
+              />
+              <BotaoLinkedin 
+              onPress={() => this.props.navigation.push('Linkedin')}
+              />
+            </View>
           </View>
-          <View style={{ ...Estilo.textInput, marginBottom: 5 }}>
-            <TextInput
-              placeholder="Senha" />
-          </View>
-          <View style={{ flexDirection: 'row',width: '100%'}}>
-            <Text>                             </Text>
-            <BotaoEsqueciSenha />
-          </View>
-          
-          <BotaoEntrar />
-          <BotaoCadastrar />
-          <View style={Estilo.line}></View>
-          <View>
-            <BotaoGoogle />
-            <BotaoLinkedin />
-          </View>
-        </View>
-      </View >
+        </View >
+      </ScrollView>
 
     );
   }
