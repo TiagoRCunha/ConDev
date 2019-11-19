68eeec51560293ccf135446ac3f6da77100eef1d
Tiago Rodrigues Cunha <tiago.r.c.sn@gmail.com>
2019-11-19 20:51:12 -0300

    adiciondo classe abstrata user e preparando UserDAO

--- backend/src/Model/User.php
+++ backend/src/Model/User.php
@@ -3,94 +3,167 @@
 namespace Model;
 
 /**
- * Class: Users
+ * abstract Class: Users
+ * @author Tiago Rodrigues Cunha
+ * 
+ * @package Model
+ * 
+ * @method getName
+ * @method getCep
+ * @method getAddress
+ * @method getPhone
+ * @method getEmail
+ * @method getPassword
+ * @method getTags
+ * @method setName()
+ * @method setCep
+ * @method setAddress
+ * @method setPhone
+ * @method setEmail
+ * @method setPassword
+ * @method setTags
  */
+abstract class User
+{
 
- class User {
-     private $id;
-     private $nome;
-     private $login;
-     private $senha; 
-
-     
+     private $name;
+     private $cep;
+     private $address;
+     private $phone;
+     private $email;
+     private $password;
+     private $tags;
 
      /**
-      * Get the value of id
-      */ 
-     public function getId()
+      * Get the value of name
+      * @return name
+      */
+     public function getName(): string
      {
-          return $this->id;
+          return $this->name;
      }
 
      /**
-      * Set the value of id
-      *
-      * @return  self
-      */ 
-     public function setId($id)
+      * Set the value of name
+      * @param string $name
+      * @return  none
+      */
+     public function setName(string $name)
      {
-          $this->id = $id;
-
-          return $this;
+          $this->name = $name;
      }
 
      /**
-      * Get the value of nome
-      */ 
-     public function getNome()
+      * Get the value of cep
+      * @return cep
+      */
+     public function getCep(): string
      {
-          return $this->nome;
+          return $this->cep;
      }
 
      /**
-      * Set the value of nome
-      *
-      * @return  self
-      */ 
-     public function setNome($nome)
+      * Set the value of cep
+      * @param string $cep
+      * @return none
+      */
+     public function setCep(string $cep)
      {
-          $this->nome = $nome;
-
-          return $this;
+          $this->cep = $cep;
      }
 
      /**
-      * Get the value of login
-      */ 
-     public function getLogin()
+      * Get the value of address
+      * @return address
+      */
+     public function getAddress(): string
      {
-          return $this->login;
+          return $this->address;
      }
 
      /**
-      * Set the value of login
-      *
-      * @return  self
-      */ 
-     public function setLogin($login)
+      * Set the value of address
+      * @param string $address
+      * @return none
+      */
+     public function setAddress(string $address)
      {
-          $this->login = $login;
-
-          return $this;
+          $this->address = $address;
      }
 
      /**
-      * Get the value of senha
-      */ 
-     public function getSenha()
+      * Get the value of phone
+      * @return phone
+      */
+     public function getPhone(): string
      {
-          return $this->senha;
+          return $this->phone;
      }
 
      /**
-      * Set the value of senha
-      *
-      * @return  self
-      */ 
-     public function setSenha($senha)
+      * Set the value of phone
+      * @param string $phone
+      * @return none
+      */
+     public function setPhone(string $phone)
      {
-          $this->senha = $senha;
-
-          return $this;
+          $this->phone = $phone;
      }
- }
+
+     /**
+      * Get the value of email
+      * @return email
+      */
+     public function getEmail(): string
+     {
+          return $this->email;
+     }
+
+     /**
+      * Set the value of email
+      * @param string $email
+      * @return none
+      */
+     public function setEmail(string $email)
+     {
+          $this->email = $email;
+     }
+
+     /**
+      * Get the value of password
+      * @return password
+      */
+     public function getPassword(): string
+     {
+          return $this->password;
+     }
+
+     /**
+      * Set the value of password
+      * @param string $password
+      * @return none
+      */
+     public function setPassword(string $password)
+     {
+          $this->password = $password;
+     }
+
+     /**
+      * Get the value of tags
+      * @return tags
+      */
+     public function getTags(): array
+     {
+          return $this->tags;
+     }
+
+     /**
+      * Set the value of tags
+      * @param string $tags
+      * @return none
+      */
+     public function setTags(array $tags)
+     {
+          $this->tags = $tags;
+     }
+}
