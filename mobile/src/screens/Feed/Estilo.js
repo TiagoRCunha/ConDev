7b39d43ac0ba2509c81865cd54ac49eeaa5bb43d
Victor Corassa <victorcorassa@MacBook-Pro-de-Victor.local>
2019-11-20 17:47:50 -0300

    Estilizando

---
+++ mobile/src/screens/Feed/Estilo.js
@@ -0,0 +1,60 @@
+import { Platform, StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+    view: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        flex: 1,
+        paddingVertical: 50
+    },
+    view1: {
+        padding: 0,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    textInput: {
+        backgroundColor: 'white',
+        borderColor: 'black',
+        borderWidth: 1,
+        borderRadius: 5,
+        width: 250
+    },
+    image: {
+        height: 100,
+        width: 100,
+        borderRadius: 50,
+        borderWidth: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginBottom: 50
+    },
+    text: {
+        fontSize: 20
+    },
+    line: {
+        width: 300,
+        borderWidth: 0.3,
+        marginTop: 50
+    },
+    esqueciSenha:{
+        alignItems: "flex-end",
+        justifyContent: 'flex-end'
+
+    },
+    textSenha: {
+        fontSize: 12
+    },
+    textoGL: {
+        marginTop: 0,
+        marginLeft: 5
+        
+    },
+    viewGL: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 20
+    },
+
+
+})
