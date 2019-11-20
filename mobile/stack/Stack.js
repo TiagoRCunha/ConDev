079e9b240a4c65f5615b7bcae6bef7c1add63ec4
Victor Corassa <victorcorassa@MacBook-Pro-de-Victor.local>
2019-11-20 17:48:38 -0300

    Configuração Drawer Navigator

--- mobile/stack/Stack.js
+++ mobile/stack/Stack.js
@@ -1,15 +1,24 @@
+import React, { Component } from 'react';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 
 //Importando Cenas
 import Home from '../src/screens/Home/Home'
+import Feed from '../src/screens/Feed/Feed'
 import TipoCadastro from '../src/screens/FluxoCadastro/TipoCadastro'
 import Desenvolvedor from '../src/screens/FluxoCadastro/Desenvolvedor'
 import Startup from '../src/screens/FluxoCadastro/Startup'
 import Google from '../src/screens/FluxoCadastro/Google'
 import Linkedin from '../src/screens/FluxoCadastro/Linkedin'
 import Senha from '../src/screens/Senha/Senha'
+import { createDrawerNavigator } from 'react-navigation-drawer';
+import CustomDrawerContentComponent from '../src/components/Drawer'
 
+const DrawerConfig = {
+    contentComponent: ({navigation}) => {
+        return(<CustomDrawerContentComponent navigation={navigation} />)
+    }
+}
 
 const AppNavigator = createStackNavigator({
     Home,
@@ -18,7 +27,8 @@ const AppNavigator = createStackNavigator({
     Google,
     Linkedin,
     Startup,
-    Senha
+    Senha,
+    Feed
 },
     {
         initialRouteName: 'Home',
@@ -27,4 +37,17 @@ const AppNavigator = createStackNavigator({
 
 );
 
-export default createAppContainer(AppNavigator);
+const DrawerNavigator = createDrawerNavigator({
+    Screen1: {
+        screen: AppNavigator
+    },
+    Screen2: {
+        screen: Senha
+    }
+    
+
+
+},
+DrawerConfig)
+
+export default createAppContainer(DrawerNavigator);
