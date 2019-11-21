757f06de17d1427995a34164e3a64358ec76571b
Victor Corassa <victorcorassa@MacBook-Pro-de-Victor.local>
2019-11-21 15:21:46 -0300

    Componente NavBar, passando props para as respectivas telas

---
+++ mobile/src/components/NavBar.js
@@ -0,0 +1,57 @@
+
+import React, { Component } from 'react';
+import {
+    View,
+    ScrollView,
+    Text,
+    Image,
+    Switch,
+    TouchableOpacity,
+    FlatList,
+    Alert,
+    StyleSheet
+} from 'react-native';
+import SafeAreaView from 'react-native-safe-area-view';
+import { DrawerItems } from 'react-navigation-drawer';
+import Estilo from './Estilo';
+import Ionicons from 'react-native-vector-icons/Ionicons'
+import { withNavigation } from 'react-navigation';
+
+const menu = <Ionicons
+    name="ios-menu"
+    size={40}
+    color='black'
+/>
+
+class NavBar extends Component {
+
+    render() {
+        return (
+
+            <View style={this.props.LarguraMenuFeed ? this.props.LarguraMenuFeed : Estilo.viewMenu}>
+                <TouchableOpacity
+                onPress={()=>{
+                    this.props.navigation.toggleDrawer()
+                }}>
+                    <View style={{
+                        marginLeft: 10
+                    }}>
+                        {menu}
+                    </View>
+                </TouchableOpacity>
+
+
+            </View>
+        )
+    }
+
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'white'
+    },
+});
+
+export default withNavigation(NavBar);
