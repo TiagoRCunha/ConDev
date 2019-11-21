d3a12cb14d3df2bff40c0e6d9e461c510efe63ff
Victor Corassa <victorcorassa@MacBook-Pro-de-Victor.local>
2019-11-21 15:22:17 -0300

    Implementando a NavBar na tela de Feed

--- mobile/src/screens/Feed/Feed.js
+++ mobile/src/screens/Feed/Feed.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { View, Text, TextInput, ScrollView, Alert } from 'react-native';
+import { View, Text, TextInput, ScrollView, Alert, TouchableOpacity } from 'react-native';
 import Estilo from './Estilo'
 import { BotaoEntrar, BotaoCadastrar, BotaoEsqueciSenha, BotaoGoogle, BotaoLinkedin } from '../../components/Button'
+import NavBar from '../../components/NavBar'
 
 
 
@@ -10,21 +11,40 @@ export default class Feed extends Component {
   render() {
     return (
       <ScrollView>
-        <ScrollView
-        style={{ borderWidth: 1, height: 50}}
-        horizontal>
-          <Text>ScrollView</Text>
-          <Text>ScrollView</Text>
-          <Text>ScrollView</Text>
-          <Text>ScrollView</Text>
-          <Text>ScrollView</Text>
-          <Text>ScrollView</Text>
-          <Text>ScrollView</Text>
-        </ScrollView>
+        
+        <View style={{
+          flexDirection: 'row',
+          width: '100%',
+          
+        }}>
+          <NavBar
+          LarguraMenuFeed={{...Estilo.viewMenu, width: '15%'}} />
+          <ScrollView
+            style={{height: 50, width: '80%' }}
+            horizontal
+            showsHorizontalScrollIndicator={false}>
+            <Text>ScrollView</Text>
+            <Text>ScrollView</Text>
+            <Text>ScrollView</Text>
+            <Text>ScrollView</Text>
+            <Text>ScrollView</Text>
+            <Text>ScrollView</Text>
+            <Text>ScrollView</Text>
+          </ScrollView>
+          
+
+        </View>
+
         <View style={Estilo.view}>
-          <Text>
-            Feed
+          <TouchableOpacity
+            onPress={() => {
+              this.props.navigation.toggleDrawer()
+            }}>
+            <Text>
+              Feed
           </Text>
+          </TouchableOpacity>
+
         </View >
       </ScrollView>
 
