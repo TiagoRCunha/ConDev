11954309c2d57c2d2efa7d07ca817e1e3a206c93
Victor Corassa <victorcorassa@MacBook-Pro-de-Victor.local>
2019-11-21 15:21:08 -0300

    ALterando propriedades do drawer, passando a tela de perfil como rota.

--- mobile/src/components/Drawer.js
+++ mobile/src/components/Drawer.js
@@ -13,26 +13,69 @@ import {
 } from 'react-native';
 import SafeAreaView from 'react-native-safe-area-view';
 import { DrawerItems } from 'react-navigation-drawer';
+import Estilo from '../screens/Home/Estilo';
+import Ionicons from 'react-native-vector-icons/Ionicons'
+import { withNavigation } from 'react-navigation';
+import NavBar from './NavBar'
+
+const person = <Ionicons
+    name="md-person"
+    size={100}
+    color='#000000'
+/>
 
 class CustomDrawerContentComponent extends Component {
     navLink(nav, text) {
         return (
             <TouchableOpacity
-            onPress={() => {
-                this.props.navigation.navigate(nav)
-            }}>
-                <Text>{text}</Text>
+                onPress={() => {
+                    this.props.navigation.navigate(nav)
+                }}>
+                <Text style={{ fontSize: 25, margin: 20 }}>{text}</Text>
             </TouchableOpacity>
         )
     }
     render() {
         return (
-            <View> 
+            <View>
                 <View style={styles.container}>
                     <Text>Teste</Text>
                 </View>
-                <View>
-                    {this.navLink('AppNavigator', 'Home')}
+                <NavBar
+                LarguraMenuFeed
+                />
+                <View style={{ height: 200, backgroundColor: 'white', alignItems: 'center', justifyContent: 'center', borderBottomWidth: 0.3 }}>
+                    {person}
+                    
+                </View>
+                <View style={{ height: 400 }}>
+                    <TouchableOpacity
+                    onPress={()=> {
+                        // this.props.navigation.push('EditarPerfil')
+                    }}>
+                        <Text style={{ fontSize: 25, margin: 20 }}>
+                            Modo escuro
+                        </Text>
+                    </TouchableOpacity>
+                    <View>
+                        {this.navLink('EditarPerfil', 'Editar perfil')}
+                    </View>
+                    <TouchableOpacity>
+                        <Text style={{ fontSize: 25, margin: 20 }}>
+                            Configurações
+                        </Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <Text style={{ fontSize: 25, margin: 20 }}>
+                            Seguidores
+                        </Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <Text style={{ fontSize: 15, margin: 20 }}>
+                            Sair
+                        </Text>
+                    </TouchableOpacity>
+
                 </View>
             </View>
         )
@@ -47,4 +90,4 @@ const styles = StyleSheet.create({
     },
 });
 
-export default CustomDrawerContentComponent
+export default withNavigation(CustomDrawerContentComponent);
