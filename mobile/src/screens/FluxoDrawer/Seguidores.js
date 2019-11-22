bcbe6563f0a7907fdaecdc966291c4e2745e3975
Victor Corassa <victorcorassa@MacBook-Pro-de-Victor.local>
2019-11-22 09:41:13 -0300

    Telas prontas

---
+++ mobile/src/screens/FluxoDrawer/Seguidores.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { View, Text, ScrollView } from 'react-native';
+import Estilo from './Estilo'
+import CustomTextInput from '../../components/CustomTextInput'
+import NavBar from '../../components/NavBar'
+
+//Botoes
+import { BotaoDesenvolvedor, BotaoStartup, BotaoVoltar, BotaoSalvar } from '../../components/Button'
+
+
+
+
+
+export default class Seguidores extends Component {
+
+    constructor(props){
+        super(props)
+    }
+
+    componentDidMount() {
+        console.log(this.props.navigation.state.routeName)
+    }
+
+    render() {
+        return (
+            <ScrollView>
+                <NavBar
+                text="Seguidores" />
+                <View style={Estilo.view}>
+                    
+                </View>
+            </ScrollView>
+
+        );
+    }
+}
