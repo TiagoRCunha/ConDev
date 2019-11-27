d513d4d6a2fc189674894a4fc5582b444832d19f
Tiago Rodrigues Cunha <tiago.r.c.sn@gmail.com>
2019-11-27 19:57:38 -0300

    ajustando manager

--- backend/src/Persistence/UserDAO.php
+++ backend/src/Persistence/UserDAO.php
@@ -5,15 +5,16 @@ namespace Persistence;
 class UserDAO extends Connection
 {
 
-  private static $manager = $this->getConnection();
+  // private static $manager = $this->getConnection();
 
   public static function getUserById(string $id)
   {
     // herdando um instancia do mongo
-    self::$manager->$arr_user = [
-      ['id' => $id, 'name' => 'teste nome']
-    ];
+    // self::$manager->
+    // $arr_user = [
+    //   ['id' => $id, 'name' => 'teste nome']
+    // ];
 
-    return json_encode($arr_user);
+    // return json_encode($arr_user);
   }
 }
