2fc0550ced4a63f351d322416ede9374933c030c
Victor Corassa <victorcorassa@MBP-de-Victor.mshome.net>
2019-11-30 11:37:51 -0300

    Importando botões

--- mobile/src/screens/FluxoDrawer/Seguidores.js
+++ mobile/src/screens/FluxoDrawer/Seguidores.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
-import { View, Text, ScrollView } from 'react-native';
+import { View, Text, ScrollView, TouchableOpacity } from 'react-native';
 import Estilo from './Estilo'
 import CustomTextInput from '../../components/CustomTextInput'
 import NavBar from '../../components/NavBar'
+import Ionicons from 'react-native-vector-icons/Ionicons'
 
 //Botoes
-import { BotaoDesenvolvedor, BotaoStartup, BotaoVoltar, BotaoSalvar } from '../../components/Button'
-
+import { BotaoDesenvolvedor, BotaoStartup, BotaoVoltar, BotaoSalvar, BotaoSeguidores } from '../../components/Button'
 
 
 
 
 export default class Seguidores extends Component {
 
-    constructor(props){
+    constructor(props) {
         super(props)
     }
 
@@ -23,12 +23,15 @@ export default class Seguidores extends Component {
 
     render() {
         return (
-            <ScrollView>
+            <ScrollView style={{ flex: 1 }}>
                 <NavBar
-                text="Seguidores" />
-                <View style={Estilo.view}>
-                    
-                </View>
+                    text="Seguidores" />
+                <BotaoSeguidores
+                onPress={()=> this.props.navigation.navigate('DetalheSeguidores')} />
+                <BotaoSeguidores />
+                <BotaoSeguidores />
+                <BotaoSeguidores />
+
             </ScrollView>
 
         );
