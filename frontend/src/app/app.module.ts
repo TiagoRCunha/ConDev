5e7cfba7ed823cb78551226e4456496c0c24993c
Victor <victorhpf97@gmail.com>
2019-12-06 21:12:49 -0300

    Adicionado
    -Editar perfil
    -Criar evento
    -Pagina com varios componentes de exemplo já importados

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,24 +8,47 @@ import {CalendarModule} from 'primeng/calendar';
 import {PickListModule} from 'primeng/picklist';
 
 //Meus imports
+
+import { NavbarComponent } from './navbar/navbar.component';
+import {MenubarModule} from 'primeng/menubar';
+import {CheckboxModule} from 'primeng/checkbox';
+import {DropdownModule} from 'primeng/dropdown';
 import {CardModule} from 'primeng/card';
 import {CarouselModule} from 'primeng/carousel';
 import {ChartModule} from 'primeng/chart';
 import {DialogModule} from 'primeng/dialog';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component'
 import { SidebarModule } from 'primeng/sidebar';
-import {MenubarModule} from 'primeng/menubar';
+import { MessageService } from 'primeng/api';
+import { ComponenteExemploComponent } from './components/componente-exemplo/componente-exemplo.component';
+import {MessagesModule} from 'primeng/messages';
+import {MessageModule} from 'primeng/message';
+import {ToastModule} from 'primeng/toast';
+import {ConfirmDialogModule} from 'primeng/confirmdialog';
+import {ConfirmationService} from 'primeng/api';
+import {TooltipModule} from 'primeng/tooltip';
+import {LightboxModule} from 'primeng/lightbox';
+import {CommonModule} from '@angular/common';
+import {TabViewModule} from 'primeng/tabview';
+import {CodeHighlighterModule} from 'primeng/codehighlighter';
+import {FileUploadModule} from 'primeng/fileupload';
+import { HttpClientModule } from '@angular/common/http';
+import { EditarPerfilComponent } from './components/editar-perfil/editar-perfil.component';
 import {InputTextModule} from 'primeng/inputtext';
-import {CheckboxModule} from 'primeng/checkbox';
-import {DropdownModule} from 'primeng/dropdown';
+import {PasswordModule} from 'primeng/password';
+import {SplitButtonModule} from 'primeng/splitbutton';
+import {InputTextareaModule} from 'primeng/inputtextarea';
+import { DivulgarComponent } from './components/divulgar/divulgar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     HomeComponent,
+    DivulgarComponent,
+    ComponenteExemploComponent,
+    EditarPerfilComponent,
   
   ],
   imports: [
@@ -43,13 +66,28 @@ import {DropdownModule} from 'primeng/dropdown';
     InputTextModule,
     CheckboxModule,
     DropdownModule,
-    
-  
+    MessagesModule,
+    MessageModule,
+    ToastModule,
+    InputTextareaModule,
+    SplitButtonModule,
+    PasswordModule,
+    HttpClientModule,
+    FileUploadModule,
+    CodeHighlighterModule,
+    TabViewModule,
+    CommonModule,
+    LightboxModule,
+    TooltipModule,
+    ConfirmDialogModule
+
   ],
   schemas: [
     CUSTOM_ELEMENTS_SCHEMA
 ],
   providers: [
+    MessageService,
+    ConfirmationService,
 
    
   ],
