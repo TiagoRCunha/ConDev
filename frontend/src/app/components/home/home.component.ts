bbd62d8dfb6d5fd8ed509a800224140e543c76ad
Caio <caioeduardoabreu@gmail.com>
2019-12-07 01:33:10 -0300

    pagina home

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -9,17 +9,17 @@ import { Title } from '@angular/platform-browser';
 export class HomeComponent implements OnInit {
 
 
-  fitas = [
-    { id: 1, nome: 'SimCity', ano: '1989', link: 'assets/img/fitas/SimCity.jpg', descricao: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse elementum hendrerit ligula, bibendum maximus elit pretium id. Sed tincidunt quis nibh a dapibus. Nullam rutrum scelerisque volutpat. Cras tincidunt fringilla enim. Mauris purus mi, tincidunt non purus ac, tincidunt faucibus sapien. Phasellus ultrices pellentesque metus, in blandit dolor congue n' },
-    { id: 2, nome: 'Top Gear', ano: '1992', link: 'assets/img/fitas/TopGear.jpg', descricao: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse elementum hendrerit ligula, bibendum maximus elit pretium id. Sed tincidunt quis nibh a dapibus. Nullam rutrum scelerisque volutpat. Cras tincidunt fringilla enim. Mauris purus mi, tincidunt non purus ac, tincidunt faucibus sapien. Phasellus ultrices pellentesque metus, in blandit dolor congue n' },
-    { id: 3, nome: 'Clay Mates', ano: '1991', link: 'assets/img/fitas/ClayMates.jpg', descricao: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse elementum hendrerit ligula, bibendum maximus elit pretium id. Sed tincidunt quis nibh a dapibus. Nullam rutrum scelerisque volutpat. Cras tincidunt fringilla enim. Mauris purus mi, tincidunt non purus ac, tincidunt faucibus sapien. Phasellus ultrices pellentesque metus, in blandit dolor congue n' },
-    { id: 5, nome: 'Batman Forever', ano: '1991', link: 'assets/img/fitas/batman.jpg', descricao: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse elementum hendrerit ligula, bibendum maximus elit pretium id. Sed tincidunt quis nibh a dapibus. Nullam rutrum scelerisque volutpat. Cras tincidunt fringilla enim. Mauris purus mi, tincidunt non purus ac, tincidunt faucibus sapien. Phasellus ultrices pellentesque metus, in blandit dolor congue n' },
-    { id: 6, nome: 'Tom and Jerry', ano: '1991', link: 'assets/img/fitas/tom.jpg', descricao: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse elementum hendrerit ligula, bibendum maximus elit pretium id. Sed tincidunt quis nibh a dapibus. Nullam rutrum scelerisque volutpat. Cras tincidunt fringilla enim. Mauris purus mi, tincidunt non purus ac, tincidunt faucibus sapien. Phasellus ultrices pellentesque metus, in blandit dolor congue n' },
-    { id: 7, nome: 'O Mascara', ano: '1991', link: 'assets/img/fitas/mask.jpg', descricao: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse elementum hendrerit ligula, bibendum maximus elit pretium id. Sed tincidunt quis nibh a dapibus. Nullam rutrum scelerisque volutpat. Cras tincidunt fringilla enim. Mauris purus mi, tincidunt non purus ac, tincidunt faucibus sapien. Phasellus ultrices pellentesque metus, in blandit dolor congue nin blandit dolor congue n' },
-    { id: 8, nome: 'James Bond jr', ano: '1991', link: 'assets/img/fitas/007.jpg', descricao: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse elementum hendrerit ligula, bibendum maximus elit pretium id. Sed tincidunt quis nibh a dapibus. Nullam rutrum scelerisque volutpat. Cras tincidunt fringilla enim. Mauris purus mi, tincidunt non purus ac, tincidunt faucibus sapien. Phasellus ultrices pellentesque metus, in blandit dolor congue n' },
+  // fitas = [
+  //   { id: 1, nome: 'SimCity', ano: '1989', link: 'assets/img/fitas/SimCity.jpg', descricao: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse elementum hendrerit ligula, bibendum maximus elit pretium id. Sed tincidunt quis nibh a dapibus. Nullam rutrum scelerisque volutpat. Cras tincidunt fringilla enim. Mauris purus mi, tincidunt non purus ac, tincidunt faucibus sapien. Phasellus ultrices pellentesque metus, in blandit dolor congue n' },
+  //   { id: 2, nome: 'Top Gear', ano: '1992', link: 'assets/img/fitas/TopGear.jpg', descricao: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse elementum hendrerit ligula, bibendum maximus elit pretium id. Sed tincidunt quis nibh a dapibus. Nullam rutrum scelerisque volutpat. Cras tincidunt fringilla enim. Mauris purus mi, tincidunt non purus ac, tincidunt faucibus sapien. Phasellus ultrices pellentesque metus, in blandit dolor congue n' },
+  //   { id: 3, nome: 'Clay Mates', ano: '1991', link: 'assets/img/fitas/ClayMates.jpg', descricao: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse elementum hendrerit ligula, bibendum maximus elit pretium id. Sed tincidunt quis nibh a dapibus. Nullam rutrum scelerisque volutpat. Cras tincidunt fringilla enim. Mauris purus mi, tincidunt non purus ac, tincidunt faucibus sapien. Phasellus ultrices pellentesque metus, in blandit dolor congue n' },
+  //   { id: 5, nome: 'Batman Forever', ano: '1991', link: 'assets/img/fitas/batman.jpg', descricao: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse elementum hendrerit ligula, bibendum maximus elit pretium id. Sed tincidunt quis nibh a dapibus. Nullam rutrum scelerisque volutpat. Cras tincidunt fringilla enim. Mauris purus mi, tincidunt non purus ac, tincidunt faucibus sapien. Phasellus ultrices pellentesque metus, in blandit dolor congue n' },
+  //   { id: 6, nome: 'Tom and Jerry', ano: '1991', link: 'assets/img/fitas/tom.jpg', descricao: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse elementum hendrerit ligula, bibendum maximus elit pretium id. Sed tincidunt quis nibh a dapibus. Nullam rutrum scelerisque volutpat. Cras tincidunt fringilla enim. Mauris purus mi, tincidunt non purus ac, tincidunt faucibus sapien. Phasellus ultrices pellentesque metus, in blandit dolor congue n' },
+  //   { id: 7, nome: 'O Mascara', ano: '1991', link: 'assets/img/fitas/mask.jpg', descricao: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse elementum hendrerit ligula, bibendum maximus elit pretium id. Sed tincidunt quis nibh a dapibus. Nullam rutrum scelerisque volutpat. Cras tincidunt fringilla enim. Mauris purus mi, tincidunt non purus ac, tincidunt faucibus sapien. Phasellus ultrices pellentesque metus, in blandit dolor congue nin blandit dolor congue n' },
+  //   { id: 8, nome: 'James Bond jr', ano: '1991', link: 'assets/img/fitas/007.jpg', descricao: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse elementum hendrerit ligula, bibendum maximus elit pretium id. Sed tincidunt quis nibh a dapibus. Nullam rutrum scelerisque volutpat. Cras tincidunt fringilla enim. Mauris purus mi, tincidunt non purus ac, tincidunt faucibus sapien. Phasellus ultrices pellentesque metus, in blandit dolor congue n' },
 
 
-  ];
+  // ];
 
   constructor() {
 
@@ -29,9 +29,9 @@ export class HomeComponent implements OnInit {
 
   }
 
-  ngOnInit() { 
-    
-}
+  ngOnInit() {
+
+  }
 
 
 }
